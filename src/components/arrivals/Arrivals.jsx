849c0983c967fd3619da1109d4e1370d3e7d04f4
Artiom_Sokolov <sokolova-net@mail.ru>
2023-05-24 14:44:42 +0300

    add cards

--- src/components/arrivals/Arrivals.jsx
+++ src/components/arrivals/Arrivals.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import Card from '../card/Card';
+
 import './Arrivals.scss';
+
+import cat01Img from './../../img/categories/cat-01.jpg';
+import cat02Img from './../../img/categories/cat-02.jpg';
+import cat03Img from './../../img/categories/cat-03.jpg';
 
 export default function Arrivals() {
   return (
@@ -9,7 +15,9 @@ export default function Arrivals() {
           <h2 className="subtitle">NEW ARRIVALS</h2>
         </div>
         <div className="arrivals__cards">
-
+          <Card title="Hoodies & Sweetshirt" img={cat01Img} />
+          <Card title="Coats & Parkas" img={cat02Img} />
+          <Card title="Tees & T-Shirt" img={cat03Img} />
         </div>
       </div>
     </section>

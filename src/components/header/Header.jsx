e10c4a8b390d502dad3a0ab8599185e0f3a59b4a
Artiom_Sokolov <sokolova-net@mail.ru>
2023-05-27 10:53:05 +0300

    media querries for header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { slide as Menu } from 'react-burger-menu'
 import './Header.scss';
-import logoImg from './../../img/icons/logo.svg'
+import logoImg from './../../img/icons/logo.svg';
 
 export default function Header() {
   return (
@@ -12,13 +13,18 @@ export default function Header() {
             <span>Fashion</span>
           </div>
           <nav className="header__nav">
-            <ul>
-              <li><a href="#!">CATALOGUE</a></li>
+            <ul className="header__nav-list">
+              <li className="header__nav-item"><a href="#!">CATALOGUE</a></li>
               <li><a href="#!">FASHION</a></li>
               <li><a href="#!">FAVOURITE</a></li>
               <li><a href="#!">LIFESTYLE</a></li>
-              <li><a href="#!" className="header__nav-btn">SIGN UP</a></li>
             </ul>
+            <Menu right itemListElement="div">
+              <a id="home" className="menu-item" href="/">CATALOGUE</a>
+              <a id="about" className="menu-item" href="/about">FASHION</a>
+              <a id="contact" className="menu-item" href="/contact">FAVOURITE</a>
+            </Menu>
+            <a href="#!" className="header__nav-btn">SIGN UP</a>
           </nav>
         </div>
       </div>
